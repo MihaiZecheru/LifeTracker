0d3a895ebc4baca313f89e60ddf803e684eb95f5
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-04-25 23:02:39 -0700

    Add documentation

--- LifeTracker/DateRange.cs
+++ LifeTracker/DateRange.cs
@@ -2,8 +2,19 @@
 
 public struct DateRange
 {
+    /// <summary>
+    /// The day the range starts with
+    /// </summary>
     public DateOnly Start { get; }
+    
+    /// <summary>
+    /// The day the range ends with
+    /// </summary>
     public DateOnly End { get; private set; }
+    
+    /// <summary>
+    /// The length of the range
+    /// </summary>
     public int Length { get; private set; }
 
     public DateRange(DateOnly start, DateOnly end, int length)
@@ -13,6 +24,12 @@ public struct DateRange
         Length = length;
     }
 
+    /// <summary>
+    /// Add a day to the <see cref="DateRange"/>
+    /// <br/><br/>
+    /// Will extend the <see cref="DateRange"/> from <see cref="DateRange.Start"/> to the given <paramref name="day"/>
+    /// </summary>
+    /// <param name="day">The day to extend the range to</param>
     public void AddDay(DateOnly day)
     {
         End = day;
