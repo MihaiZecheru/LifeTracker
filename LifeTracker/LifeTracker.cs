7024984a6be42c6154bed8a1b6abe83cf510c4b8
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-05-01 23:42:20 -0700

    Work on integrating EntryEditor popup

--- LifeTracker/LifeTracker.cs
+++ LifeTracker/LifeTracker.cs
@@ -1,4 +1,5 @@
 ﻿using Spectre.Console;
+using System.Net;
 
 namespace LifeTracker;
 
@@ -23,12 +24,21 @@ public class LifeTracker
     }
 
     /// <summary>
+    /// When false, the <see cref="StartKeypressListener"/> will stop listening for keypress events
+    /// <br/><br/>
+    /// Used for pausing the LifeTracker console screen while the <see cref="EntryEditor"/> is open
+    /// </summary>
+    private static bool ListenForKeypress = true;
+
+    /// <summary>
     /// Listen for arrow keypress events in order to move the user's cursor on the <see cref="Calendar"/>
     /// </summary>
     private static void StartKeypressListener()
     {
         while (true)
         {
+            if (!ListenForKeypress) continue;
+
             ConsoleKeyInfo keyinfo = Console.ReadKey(true);
             bool ctrl = keyinfo.Modifiers.HasFlag(ConsoleModifiers.Control);
             bool shift = keyinfo.Modifiers.HasFlag(ConsoleModifiers.Shift);
@@ -52,10 +62,87 @@ public class LifeTracker
                     break;
 
                 case ConsoleKey.Enter:
-                    if (ActiveCalendar.Get())
+                    DateOnly date = ActiveCalendar.SelectedDate();
 
+                    // Edit existing entry if it exists
+                    if (ActiveCalendar.Get(date) != null)
+                    {
+                        EditEntry(ActiveCalendar.Get(ActiveCalendar.SelectedDate());
+                    }
+                    // Only create new entry if the entry is from today
+                    else if (date == DateOnly.FromDateTime(DateTime.Today))
+                    {
+                        CreateEntry(date);
+                    }
+                    
                     break;
             }
         }
     }
+
+    /// <summary>
+    /// Open the <see cref="EntryEditor"/>
+    /// </summary>
+    private static void OpenEntryEditor()
+    {
+        EntryEditor.Program.Main();
+    }
+
+    /// <summary>
+    /// Edit an existing <see cref="Entry"/> using the <see cref="EntryEditor"/> WinForm
+    /// </summary>
+    /// <param name="entry">The existing <see cref="Entry"/> to edit</param>
+    private static void EditEntry(Entry entry)
+    {
+        OpenEntryEditor(); // TODO: send the entry OSS & DS to the winform
+        ListenForFormEvents(entry.For);
+    }
+
+    /// <summary>
+    /// Create an <see cref="Entry"/> using the <see cref="EntryEditor"/> WinForm
+    /// </summary>
+    /// <param name="date">The date to create the <see cref="Entry"/> on</param>
+    private static void CreateEntry(DateOnly date)
+    {
+        OpenEntryEditor();
+        ListenForFormEvents(date);
+    }
+
+    /// <summary>
+    /// Listen for events coming from the <see cref="EntryEditor"/> WinForm
+    /// <br/><br/>
+    /// If the status code is 200, meaning the user has finished editing his <see cref="Entry"/>, the entry is parsed from the response, saved, and added to the <see cref="ActiveCalendar"/><br/>
+    /// Otherwise, if the status code is 100, meaning the user has closed out of the <see cref="EntryEditor"/>, the <see cref="StartKeypressListener"/> is resumed
+    /// </summary>
+    private static void ListenForFormEvents(DateOnly date)
+    {
+        using var listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8001/");
+
+        listener.Start();
+
+        HttpListenerContext ctx = listener.GetContext();
+        HttpListenerRequest req = ctx.Request;
+
+        // EntryEditor has returned the written entry as a string in the format of: "oneSentenceSummary{<@SEP>}detailedSummary"
+        if (ctx.Response.StatusCode == 200)
+        {
+            string text;
+            using (var reader = new StreamReader(req.InputStream, req.ContentEncoding))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            Entry entry = Entry.ParseFromString(text, date);
+            ActiveCalendar.SetEntry(entry);
+        }
+        // EntryEditor has been closed
+        else if (ctx.Response.StatusCode == 100)
+        {
+            ListenForKeypress = true;
+        }
+    }
 }
+
+// TODO: create the winform and have it send a message to the http listener which contains the entry info (entry.ToString() is the format)
+// https://zetcode.com/csharp/httplistener/
