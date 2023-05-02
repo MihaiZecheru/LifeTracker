a37cdd761d52cae2a4ca1a94c2479f13ba432382
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-05-01 23:42:04 -0700

    Make Entry struct into a class

--- LifeTracker/Entry.cs
+++ LifeTracker/Entry.cs
@@ -1,7 +1,12 @@
 ﻿namespace LifeTracker;
 
-public struct Entry
+public class Entry
 {
+    /// <summary>
+    /// The set of chars used to separate the <see cref="OneSentenceSummary"/> and <see cref="DetailedSummary"/> in the file containing the <see cref="Entry"/>
+    /// </summary>
+    public static readonly string SEP = "{<@SEP>}";
+
     /// <summary>
     /// One-sentence summary of what happened on this day
     /// </summary>
@@ -15,10 +20,49 @@ public struct Entry
     /// </summary>
     public DateOnly For { get; }
 
+    /// <summary>
+    /// Create an <see cref="Entry"/> object
+    /// </summary>
+    /// <param name="oneSentenceSummary">A one-sentence summary of what happened on that day</param>
+    /// <param name="detailedSummary">A more in-depth summary of what happened on that day</param>
+    /// <param name="_for">The day this entry is for</param>
     public Entry(string oneSentenceSummary, string detailedSummary, DateOnly _for)
     {
         OneSentenceSummary = oneSentenceSummary;
         DetailedSummary = detailedSummary;
         For = _for;
+    }
+
+    /// <summary>
+    /// Load an <see cref="Entry"/> object from a file
+    /// </summary>
+    /// <param name="filepath">The path to the <see cref="Entry"/> in the filesystem</param>
+    /// <param name="date">The date the <see cref="Entry"/> was written on</param>
+    public Entry(string filepath, DateOnly date)
+    {
+        string[] lines = File.ReadAllText(filepath).Split(SEP);
+        OneSentenceSummary = lines[0];
+        DetailedSummary = lines[1];
+        For = date;
+    }
+
+    /// <summary>
+    /// Create an <see cref="Entry"/> object from a string containing the <see cref="OneSentenceSummary"/> and <see cref="DetailedSummary"/> separated by <see cref="SEP"/>
+    /// </summary>
+    /// <param name="entryString">The string to parse</param>
+    /// <param name="date">The date to assign the <see cref="Entry"/></param>
+    /// <returns>The parsed <see cref="Entry"/></returns>
+    public static Entry ParseFromString(string entryString, DateOnly date)
+    {
+        string[] lines = entryString.Split(SEP);
+        return new Entry(lines[0], lines[1], date);
+    }
+
+    /// <summary>
+    /// Convert the <see cref="Entry"/> to a string for saving to file
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{OneSentenceSummary}{SEP}{DetailedSummary}";
     }
 }
